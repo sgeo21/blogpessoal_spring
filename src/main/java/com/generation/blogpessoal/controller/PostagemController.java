dc4c32c269e0a177ae83c419747473175128cd5e
Geovana <s.geo2112@gmail.com>
2025-10-14 12:10:56 -0300

    :sparkles: Finalização do CRUD do Recurso Postagem

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,16 +1,27 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping ("/postagens")
@@ -32,4 +43,45 @@ public class PostagemController {
 		//SEleCT *FROM tb_postagens;
 		
 	}
+	@GetMapping("/{id}") // get mapping é uma saída quando procura pelo id
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){ // indica que id virá da url
+		return postagemRepository.findById(id) //busca a postagem por id
+         .map(resposta -> ResponseEntity.ok(resposta)) //se encontrar responde com ok
+         .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build()); // responde que não foi encontrado
+		        //.orElse(ResponseEntity.notFound().build()); // pode ser o anterior ou esse.
+		//SELECT * FROM tb_postagem Where id = id;
+	}
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getAllByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+		
+      }
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+	postagem.setId(null); //para garantir que a postagem será nova
+	return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+	@PutMapping
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+		
+	return postagemRepository.findById(postagem.getId())
+	          .map(resposta -> ResponseEntity.status(HttpStatus.OK)
+	        		  .body(postagemRepository.save(postagem)))
+	          .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	         	  
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete (@PathVariable Long id) {
+	   Optional<Postagem> postagem = postagemRepository.findById(id);
+	   
+	   if(postagem.isEmpty())
+		   throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+	   postagemRepository.deleteById(id);
+	   
+	 
+	}
 }
+
+
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,15 +1,21 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.blogpessoal.model.Postagem;
 
-public interface PostagemRepository extends JpaRepository<Postagem, Long>{ 
-	//tudo o que for utilizar postagem repository, vai usar o tb_postagem 
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	// tudo o que for utilizar postagem repository, vai usar o tb_postagem
 	// quem é a entidade e quem é i ID.
 	// se tiver mais de um long ele daria erro, ai neste caso deveria ser usado Id.
-	
 
-	
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+
+    //SELECT*FROM tb_postagens WHERE titulo LIKE "%?%";
+	// find = Seletc, All = *, By = WHERE, Titulo = titulo, Containing = like, 
+	//Ignore case = não diferenciar maiúsculo de minúsculo.
+
 	
 }
